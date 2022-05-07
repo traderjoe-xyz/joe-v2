5f4089aacc3df9a1682529f5ccb61e85cff132cc
Louis Myself <louismemyself@gmail.com>
2022-05-07 20:07:20 +0200

    Moved math functions inside Math.sol

---
+++ contracts/libraries/Math.sol
@@ -0,0 +1,288 @@
+/// @notice Emitted when the result overflows uint256.
+error PRBMath__MulDivOverflow(uint256 prod1, uint256 denominator);
+
+library Math {
+    /// @notice Returns the number of decimal of x
+    /// @param x The value as an uint256
+    /// @return decimals The number of decimals of x
+    function getDecimals(uint256 x) internal pure returns (uint256 decimals) {
+        unchecked {
+            if (x >= 1e38) {
+                x /= 1e38;
+                decimals += 38;
+            }
+
+            if (x >= 1e19) {
+                x /= 1e19;
+                decimals += 19;
+            }
+
+            if (x >= 1e10) {
+                x /= 1e10;
+                decimals += 10;
+            }
+
+            if (x >= 1e5) {
+                x /= 1e5;
+                decimals += 5;
+            }
+
+            if (x >= 1e3) {
+                x /= 1e3;
+                decimals += 3;
+            }
+
+            if (x >= 1e2) {
+                x /= 1e2;
+                decimals += 2;
+            }
+
+            if (x >= 1e1) {
+                decimals += 1;
+            }
+        }
+    }
+
+    /// @notice Returns a tuple (uint256 id, bool found),
+    /// id is the index of the closest bit on the right of x that is non null
+    /// @param x The value as an uint256
+    /// @param bit The index of the bit to start searching at
+    /// @return id The index of the closest non null bit on the right of x
+    /// @return found If the index was found
+    function closestBitRight(uint256 x, uint256 bit)
+        internal
+        pure
+        returns (uint256 id, bool found)
+    {
+        unchecked {
+            x <<= 255 - bit;
+
+            if (x == 0) {
+                return (0, false);
+            }
+
+            return (mostSignificantBit(x) - (255 - bit), true);
+        }
+    }
+
+    /// @notice Returns a tuple (uint256 id, bool found),
+    /// id is the index of the closest bit on the left of x that is non null
+    /// @param x The value as an uint256
+    /// @param bit The index of the bit to start searching at
+    /// @return id The index of the closest non null bit on the left of x
+    /// @return found If the index was found
+    function closestBitLeft(uint256 x, uint256 bit)
+        internal
+        pure
+        returns (uint256 id, bool found)
+    {
+        unchecked {
+            x >>= bit;
+
+            if (x == 0) {
+                return (0, false);
+            }
+
+            return (leastSignificantBit(x) + bit, true);
+        }
+    }
+
+    /// @notice Returns the index of the most significant bit of x
+    /// @param x The value as an uint256
+    /// @return msb The index of the most significant bit of x
+    function mostSignificantBit(uint256 x) internal pure returns (uint256 msb) {
+        unchecked {
+            if (x >= 1 << 128) {
+                x >>= 128;
+                msb += 128;
+            }
+            if (x >= 1 << 64) {
+                x >>= 64;
+                msb += 64;
+            }
+            if (x >= 1 << 32) {
+                x >>= 32;
+                msb += 32;
+            }
+            if (x >= 1 << 16) {
+                x >>= 16;
+                msb += 16;
+            }
+            if (x >= 1 << 8) {
+                x >>= 8;
+                msb += 8;
+            }
+            if (x >= 1 << 4) {
+                x >>= 4;
+                msb += 4;
+            }
+            if (x >= 1 << 2) {
+                x >>= 2;
+                msb += 2;
+            }
+            if (x >= 1 << 1) {
+                msb += 1;
+            }
+        }
+    }
+
+    /// @notice Returns the index of the least significant bit of x
+    /// @param x The value as an uint256
+    /// @return lsb The index of the least significant bit of x
+    function leastSignificantBit(uint256 x)
+        internal
+        pure
+        returns (uint256 lsb)
+    {
+        unchecked {
+            if (x << 128 != 0) {
+                x <<= 128;
+                lsb += 128;
+            }
+            if (x << 64 != 0) {
+                x <<= 64;
+                lsb += 64;
+            }
+            if (x << 32 != 0) {
+                x <<= 32;
+                lsb += 32;
+            }
+            if (x << 16 != 0) {
+                x <<= 16;
+                lsb += 16;
+            }
+            if (x << 8 != 0) {
+                x <<= 8;
+                lsb += 8;
+            }
+            if (x << 4 != 0) {
+                x <<= 4;
+                lsb += 4;
+            }
+            if (x << 2 != 0) {
+                x <<= 2;
+                lsb += 2;
+            }
+            if (x << 1 != 0) {
+                lsb += 1;
+            }
+
+            return 255 - lsb;
+        }
+    }
+
+    /// @notice Calculates ceil(x*y÷denominator) with full precision.
+    ///
+    /// @dev Credit to Remco Bloemen under MIT license https://xn--2-umb.com/21/muldiv.
+    /// Its algorithm was slightly tweaked to round up the mulDiv.
+    ///
+    /// Requirements:
+    /// - The denominator cannot be zero.
+    /// - The result must fit within uint256.
+    ///
+    /// Caveats:
+    /// - This function does not work with fixed-point numbers.
+    ///
+    /// @param x The multiplicand as an uint256.
+    /// @param y The multiplier as an uint256.
+    /// @param denominator The divisor as an uint256.
+    /// @return result The result as an uint256.
+    function mulDivRoundUp(
+        uint256 x,
+        uint256 y,
+        uint256 denominator
+    ) internal pure returns (uint256 result) {
+        // 512-bit multiply [prod1 prod0] = x * y. Compute the product mod 2^256 and mod 2^256 - 1, then use
+        // use the Chinese Remainder Theorem to reconstruct the 512 bit result. The result is stored in two 256
+        // variables such that product = prod1 * 2^256 + prod0.
+        uint256 prod0; // Least significant 256 bits of the product
+        uint256 prod1; // Most significant 256 bits of the product
+        assembly {
+            let mm := mulmod(x, y, not(0))
+            prod0 := mul(x, y)
+            prod1 := sub(sub(mm, prod0), lt(mm, prod0))
+        }
+
+        // Handle non-overflow cases, 256 by 256 division.
+        if (prod1 == 0) {
+            if (prod0 != 0) {
+                // Handle underflow
+                unchecked {
+                    // This can't overflow
+                    result = (prod0 - 1) / denominator + 1; // Rounds up the division
+                }
+            }
+            return result;
+        }
+
+        // Make sure the result is less than 2^256. Also prevents denominator == 0.
+        if (prod1 >= denominator) {
+            revert PRBMath__MulDivOverflow(prod1, denominator);
+        }
+
+        ///////////////////////////////////////////////
+        // 512 by 256 division.
+        ///////////////////////////////////////////////
+
+        // Make division exact by subtracting the remainder from [prod1 prod0].
+        uint256 remainder;
+        assembly {
+            // Compute remainder using mulmod.
+            remainder := mulmod(x, y, denominator)
+            result := sub(1, iszero(remainder)) // Rounds up the division
+
+            // Subtract 256 bit number from 512 bit number.
+            prod1 := sub(prod1, gt(remainder, prod0))
+            prod0 := sub(prod0, remainder)
+        }
+
+        // Factor powers of two out of denominator and compute largest power of two divisor of denominator. Always >= 1.
+        // See https://cs.stackexchange.com/q/138556/92363.
+        unchecked {
+            // Does not overflow because the denominator cannot be zero at this stage in the function.
+            uint256 lpotdod = denominator & (~denominator + 1);
+            assembly {
+                // Divide denominator by lpotdod.
+                denominator := div(denominator, lpotdod)
+
+                // Divide [prod1 prod0] by lpotdod.
+                prod0 := div(prod0, lpotdod)
+
+                // Flip lpotdod such that it is 2^256 / lpotdod. If lpotdod is zero, then it becomes one.
+                lpotdod := add(div(sub(0, lpotdod), lpotdod), 1)
+            }
+
+            // Shift in bits from prod1 into prod0.
+            prod0 |= prod1 * lpotdod;
+
+            // Invert denominator mod 2^256. Now that denominator is an odd number, it has an inverse modulo 2^256 such
+            // that denominator * inv = 1 mod 2^256. Compute the inverse by starting with a seed that is correct for
+            // four bits. That is, denominator * inv = 1 mod 2^4.
+            uint256 inverse = (3 * denominator) ^ 2;
+
+            // Use the Newton-Raphson iteration to improve the precision. Thanks to Hensel's lifting lemma, this also works
+            // in modular arithmetic, doubling the correct bits in each step.
+            inverse *= 2 - denominator * inverse; // inverse mod 2^8
+            inverse *= 2 - denominator * inverse; // inverse mod 2^16
+            inverse *= 2 - denominator * inverse; // inverse mod 2^32
+            inverse *= 2 - denominator * inverse; // inverse mod 2^64
+            inverse *= 2 - denominator * inverse; // inverse mod 2^128
+            inverse *= 2 - denominator * inverse; // inverse mod 2^256
+
+            // Because the division is now exact we can divide by multiplying with the modular inverse of denominator.
+            // This will give us the correct result modulo 2^256. Since the preconditions guarantee that the outcome is
+            // less than 2^256, this is the final result. We don't need to compute the high bits of the result and prod1
+            // is no longer required.
+            result += prod0 * inverse;
+            return result;
+        }
+    }
+
+    /// @notice Returns x on uint112 and check that it does not overflow
+    /// @param x The value as an uint256
+    /// @return The value as an uint112
+    function safe112(uint256 x) internal pure returns (uint112) {
+        require(x < 2**112, "LBE: Exceeds 112 bits");
+        return uint112(x);
+    }
+}

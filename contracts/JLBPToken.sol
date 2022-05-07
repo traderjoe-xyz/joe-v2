b0995e2661f92502a5d126b99dfd24f9c0a658ab
Louis Myself <louismemyself@gmail.com>
2022-05-07 20:40:08 +0200

    Added comments

--- contracts/JLBPToken.sol
+++ contracts/JLBPToken.sol
@@ -2,83 +2,104 @@
 
 pragma solidity 0.8.9;
 
-contract JLBPToken {
+import "./interfaces/IJLBPToken.sol";
+
+contract JLBPToken is IJLBPToken {
+    // Mapping from token ID to account balances
     mapping(uint256 => mapping(address => uint256)) private _balances;
 
+    // Mapping from account to operator approvals
     mapping(address => mapping(address => bool)) private _operatorApprovals;
 
-    mapping(uint256 => uint256) internal _totalSupply;
+    // Mapping from token ID to total supplies
+    mapping(uint256 => uint256) internal _totalSupplies;
 
     string public constant name = "JLBP Token";
     string public constant symbol = "JLBP";
 
-    event TransferSingle(
-        address indexed from,
-        address indexed to,
-        uint256 id,
-        uint256 amount
-    );
-
-    event TransferFromSingle(
-        address indexed operator,
-        address indexed from,
-        address indexed to,
-        uint256 id,
-        uint256 amount
-    );
-
-    event ApprovalForAll(
-        address indexed account,
-        address indexed operator,
-        bool approved
-    );
-
+    /// @notice Returns the number of decimals used to get its user representation
+    /// @return The number of decimals as uint8
     function decimals() external pure virtual returns (uint8) {
         return 18;
     }
 
-    function totalSupply(uint256 id) external view virtual returns (uint256) {
-        return _totalSupply[id];
+    /// @notice Returns the number of decimals used to get its user representation
+    /// @param id The token ID
+    /// @return The total supply of that token ID
+    function totalSupply(uint256 id)
+        external
+        view
+        virtual
+        override
+        returns (uint256)
+    {
+        return _totalSupplies[id];
     }
 
+    /// @notice Returns the amount of tokens of type `id` owned by `account`
+    /// @param account The address of the owner
+    /// @param id The token ID
+    /// @return The amount of tokens of type `id` owned by `account`
     function balanceOf(address account, uint256 id)
         external
         view
         virtual
+        override
         returns (uint256)
     {
         return _balances[id][account];
     }
 
+    /// @notice Transfers `amount` tokens of type `id` from `msg.sender` to `to`
+    /// @param to The address of the recipient
+    /// @param id The token ID
+    /// @param amount The amount to send
     function safeTransfer(
         address to,
         uint256 id,
         uint256 amount
-    ) external virtual {
+    ) external virtual override {
         address owner = msg.sender;
         _safeTransfer(owner, to, id, amount);
         emit TransferSingle(owner, to, id, amount);
     }
 
+    /// @notice Returns true if `operator` is approved to transfer `account`'s tokens
+    /// @param owner The address of the owner
+    /// @param spender The address of the spender
+    /// @return True if `operator` is approved to transfer `account`'s tokens
     function isApprovedForAll(address owner, address spender)
         external
         view
         virtual
+        override
         returns (bool)
     {
         return _isApprovedForAll(owner, spender);
     }
 
-    function setApprovalForAll(address operator, bool approved) external {
+    /// @notice Grants or revokes permission to `operator` to transfer the caller's tokens, according to `approved`
+    /// @param operator The address of the operator
+    /// @param approved The boolean value to grant or revoke permission
+    function setApprovalForAll(address operator, bool approved)
+        external
+        virtual
+        override
+    {
         _setApprovalForAll(msg.sender, operator, approved);
     }
 
+    /// @notice Transfers `amount` tokens of type `id` from `from` to `to`
+    /// @param from The address of the owner of the tokens
+    /// @param to The address of the recipient
+    /// @param id The token ID
+    /// @param amount The amount to send
     function safeTransferFrom(
         address from,
         address to,
         uint256 id,
         uint256 amount
-    ) external virtual {
+    ) external virtual override {
         address operator = msg.sender;
         require(
             _isApprovedForAll(from, operator),
@@ -88,12 +109,17 @@ contract JLBPToken {
         emit TransferFromSingle(msg.sender, from, to, id, amount);
     }
 
+    /// @dev Transfers `amount` tokens of type `id` from `from` to `to`
+    /// @param from The address of the owner of the tokens
+    /// @param to The address of the recipient
+    /// @param id The token ID
+    /// @param amount The amount to send
     function _safeTransfer(
         address from,
         address to,
         uint256 id,
         uint256 amount
-    ) internal {
+    ) internal virtual {
         require(from != address(0), "JLBP: transfer from the zero address");
         require(to != address(0), "JLBP: transfer to the zero address");
 
@@ -105,23 +131,31 @@ contract JLBPToken {
         _balances[id][to] += amount;
     }
 
+    /// @dev Creates `amount` tokens of type `id`, and assigns them to `account`
+    /// @param account The address of the recipient
+    /// @param id The token ID
+    /// @param amount The amount to create
     function _mint(
         address account,
         uint256 id,
         uint256 amount
-    ) internal {
+    ) internal virtual {
         require(account != address(0), "JLBP: mint to the zero address");
 
-        _totalSupply[id] += amount;
+        _totalSupplies[id] += amount;
         _balances[id][account] += amount;
         emit TransferSingle(address(0), account, id, amount);
     }
 
+    /// @dev Destroys `amount` tokens of type `id` from `account`
+    /// @param account The address of the owner
+    /// @param id The token ID
+    /// @param amount The amount to destroy
     function _burn(
         address account,
         uint256 id,
         uint256 amount
-    ) internal {
+    ) internal virtual {
         require(account != address(0), "JLBP: burn from the zero address");
 
         uint256 accountBalance = _balances[id][account];
@@ -129,24 +163,32 @@ contract JLBPToken {
         unchecked {
             _balances[id][account] = accountBalance - amount;
         }
-        _totalSupply[id] -= amount;
+        _totalSupplies[id] -= amount;
 
         emit TransferSingle(account, address(0), id, amount);
     }
 
+    /// @notice Grants or revokes permission to `operator` to transfer the caller's tokens, according to `approved`
+    /// @param operator The address of the operator
+    /// @param approved The boolean value to grant or revoke permission
     function _setApprovalForAll(
         address owner,
         address operator,
         bool approved
-    ) internal {
+    ) internal virtual {
         require(owner != operator, "JLBP: setting approval status for self");
         _operatorApprovals[owner][operator] = approved;
         emit ApprovalForAll(owner, operator, approved);
     }
 
+    /// @notice Returns true if `operator` is approved to transfer `account`'s tokens
+    /// @param owner The address of the owner
+    /// @param spender The address of the spender
+    /// @return True if `operator` is approved to transfer `account`'s tokens
     function _isApprovedForAll(address owner, address spender)
         internal
         view
+        virtual
         returns (bool)
     {
         return _operatorApprovals[owner][spender];

133fe32cb5e5c0177f6a27950886628ba91204b0
Louis Myself <louismemyself@gmail.com>
2022-05-07 20:44:12 +0200

    JLBP interfaces implemented

---
+++ contracts/interfaces/IJLBPToken.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.9;
+
+interface IJLBPToken {
+    event TransferSingle(
+        address indexed from,
+        address indexed to,
+        uint256 id,
+        uint256 amount
+    );
+
+    event TransferFromSingle(
+        address indexed operator,
+        address indexed from,
+        address indexed to,
+        uint256 id,
+        uint256 amount
+    );
+
+    event ApprovalForAll(
+        address indexed account,
+        address indexed operator,
+        bool approved
+    );
+
+    function balanceOf(address account, uint256 id)
+        external
+        view
+        returns (uint256);
+
+    function totalSupply(uint256 id) external view returns (uint256);
+
+    function safeTransfer(
+        address to,
+        uint256 id,
+        uint256 amount
+    ) external;
+
+    function isApprovedForAll(address owner, address spender)
+        external
+        view
+        returns (bool);
+
+    function setApprovalForAll(address operator, bool approved) external;
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 id,
+        uint256 amount
+    ) external;
+}

75eebd933c1ee8ba7f3cec0a7421766becd23ee0
0x0Louis <louis@joepegs.com>
2022-07-07 03:00:14 +0200

    Fix test, need to claim fees to check balances

--- test/LBPair.Liquidity.t.sol
+++ test/LBPair.Liquidity.t.sol
@@ -123,6 +123,7 @@ contract LiquidityBinPairLiquidityTest is TestHelper {
         vm.startPrank(BOB);
         pair.safeBatchTransferFrom(BOB, address(pair), _ids, amounts);
         pair.burn(_ids, amounts, BOB);
+        pair.collectFees(BOB, _ids); // the excess token were sent to fees, so they need to be claimed
         vm.stopPrank();
 
         assertEq(token6D.balanceOf(BOB), amount0In);

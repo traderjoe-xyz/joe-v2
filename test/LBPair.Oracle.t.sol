2b8de677288c98dc249ab6cb80e91705bb5f2289
0x0Louis <louis@joepegs.com>
2022-07-20 12:15:38 +0200

    test with 100 samples

--- test/LBPair.Oracle.t.sol
+++ test/LBPair.Oracle.t.sol
@@ -99,4 +99,47 @@ contract LiquidityBinPairOracleTest is TestHelper {
         assertEq(cumulativeAccumulator, 0);
         assertEq(cumulativeBinCrossed, 0);
     }
+
+    function testOracleSampleAtWith100Samples() public {
+        uint256 amount1In = 101e18;
+        (
+            uint256[] memory _ids,
+            uint256[] memory _distributionX,
+            uint256[] memory _distributionY,
+            uint256 amount0In
+        ) = spreadLiquidity(amount1In * 2, ID_ONE, 99, 100);
+
+        token6D.mint(address(pair), amount0In);
+        token18D.mint(address(pair), amount1In);
+
+        pair.mint(_ids, _distributionX, _distributionY, DEV);
+        pair.increaseOracleLength(100);
+
+        uint256 startTimestamp;
+
+        for (uint256 i; i < 200; ++i) {
+            if (i < 200) token6D.mint(address(pair), 1e18);
+            else token18D.mint(address(pair), 1e18);
+
+            vm.prank(DEV);
+            vm.warp(1500 + 100 * i);
+            pair.swap(true, DEV);
+
+            if (i == 1) startTimestamp = block.timestamp;
+        }
+
+        (uint256 cId, uint256 cAcc, uint256 cBin) = pair.getOracleSampleFrom(0);
+
+        for (uint256 i; i < 99; ++i) {
+            uint256 _ago = ((block.timestamp - startTimestamp) * i) / 100;
+
+            (uint256 cumulativeId, uint256 cumulativeAccumulator, uint256 cumulativeBinCrossed) = pair
+                .getOracleSampleFrom(_ago);
+            assertGe(cId, cumulativeId);
+            assertGe(cAcc, cumulativeAccumulator);
+            assertGe(cBin, cumulativeBinCrossed);
+
+            (cId, cAcc, cBin) = (cumulativeId, cumulativeAccumulator, cumulativeBinCrossed);
+        }
+    }
 }

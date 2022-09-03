808324bf542061b4c3eb5b192c8758abc214046c
0x0Louis <louis@joepegs.com>
2022-09-03 18:06:37 +0200

    issue n°31,77,126: declare events in the interface

--- src/interfaces/IPendingOwnable.sol
+++ src/interfaces/IPendingOwnable.sol
@@ -3,6 +3,9 @@
 pragma solidity >=0.8.7;
 
 interface IPendingOwnable {
+    event PendingOwnerSet(address indexed pendingOwner);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
     function owner() external view returns (address);
 
     function pendingOwner() external view returns (address);

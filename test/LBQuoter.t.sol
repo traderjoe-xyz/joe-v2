0eb35cf16705772187efc643929cee14ab2bb337
pecio222 <pecio222@gmail.com>
2022-09-15 13:33:07 +0200

    add test for LBQuoter_InvalidLength

--- test/LBQuoter.t.sol
+++ test/LBQuoter.t.sol
@@ -84,4 +84,13 @@ contract LiquidityBinQuoterTest is TestHelper {
     function getIdFromPrice(uint256 _price, uint16 _binStep) internal pure returns (uint24 id) {
         id = BinHelper.getIdFromPrice(_price, _binStep);
     }
+
+    function testInvalidLength() public {
+        address[] memory route;
+        route = new address[](1);
+        vm.expectRevert(LBQuoter_InvalidLength.selector);
+        quoter.findBestPathAmountIn(route, 1e18);
+        vm.expectRevert(LBQuoter_InvalidLength.selector);
+        quoter.findBestPathAmountOut(route, 20e6);
+    }
 }

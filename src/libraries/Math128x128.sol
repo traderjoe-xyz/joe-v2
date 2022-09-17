9baf11cd4e9110f22c1f3c3db9e25305ea994b48
0x0Louis <louis@joepegs.com>
2022-09-17 21:29:41 +0200

    issue 203: power could overflow and optimization

--- src/libraries/Math128x128.sol
+++ src/libraries/Math128x128.sol
@@ -81,74 +81,117 @@ library Math128x128 {
         }
     }
 
-    /// @notice Returns the value of x^y It's calculated using `1 / x^(-y)` to have the same precision
-    /// whether `y` is negative or positive.
+    /// @notice Returns the value of x^y. It calculates `1 / x^abs(y)` if x is bigger than 2^128.
+    /// At the end of the operations, we invert the result if needed.
     /// @param x The unsigned 128.128-binary fixed-point number for which to calculate the power
-    /// @param y A relative number without any decimals
-    /// @return The result of `x^y`
-    function power(uint256 x, int256 y) internal pure returns (uint256) {
-        unchecked {
-            uint256 result = Constants.SCALE;
+    /// @param y A relative number without any decimals, needs to be between ]2^20; 2^20[
+    /// @return result The result of `x^y`
+    function power(uint256 x, int256 y) internal pure returns (uint256 result) {
+        bool invert;
+        uint256 absY;
 
-            uint256 absY;
-            uint256 pow;
-            bool invert;
-
-            if (y < 0) {
-                invert = !invert;
-                absY = uint256(-y);
-            } else {
-                absY = uint256(y);
+        assembly {
+            absY := y
+            if slt(absY, 0) {
+                absY := add(not(absY), 1)
+                invert := not(invert)
             }
-
-            if (x > type(uint128).max) {
-                pow = type(uint256).max / x;
-                invert = !invert;
-            } else pow = x;
-
-            if (absY & 0x1 != 0) result = (result * pow) >> Constants.SCALE_OFFSET;
-            pow = (pow * pow) >> Constants.SCALE_OFFSET;
-            if (absY & 0x2 != 0) result = (result * pow) >> Constants.SCALE_OFFSET;
-            pow = (pow * pow) >> Constants.SCALE_OFFSET;
-            if (absY & 0x4 != 0) result = (result * pow) >> Constants.SCALE_OFFSET;
-            pow = (pow * pow) >> Constants.SCALE_OFFSET;
-            if (absY & 0x8 != 0) result = (result * pow) >> Constants.SCALE_OFFSET;
-            pow = (pow * pow) >> Constants.SCALE_OFFSET;
-            if (absY & 0x10 != 0) result = (result * pow) >> Constants.SCALE_OFFSET;
-            pow = (pow * pow) >> Constants.SCALE_OFFSET;
-            if (absY & 0x20 != 0) result = (result * pow) >> Constants.SCALE_OFFSET;
-            pow = (pow * pow) >> Constants.SCALE_OFFSET;
-            if (absY & 0x40 != 0) result = (result * pow) >> Constants.SCALE_OFFSET;
-            pow = (pow * pow) >> Constants.SCALE_OFFSET;
-            if (absY & 0x80 != 0) result = (result * pow) >> Constants.SCALE_OFFSET;
-            pow = (pow * pow) >> Constants.SCALE_OFFSET;
-            if (absY & 0x100 != 0) result = (result * pow) >> Constants.SCALE_OFFSET;
-            pow = (pow * pow) >> Constants.SCALE_OFFSET;
-            if (absY & 0x200 != 0) result = (result * pow) >> Constants.SCALE_OFFSET;
-            pow = (pow * pow) >> Constants.SCALE_OFFSET;
-            if (absY & 0x400 != 0) result = (result * pow) >> Constants.SCALE_OFFSET;
-            pow = (pow * pow) >> Constants.SCALE_OFFSET;
-            if (absY & 0x800 != 0) result = (result * pow) >> Constants.SCALE_OFFSET;
-            pow = (pow * pow) >> Constants.SCALE_OFFSET;
-            if (absY & 0x1000 != 0) result = (result * pow) >> Constants.SCALE_OFFSET;
-            pow = (pow * pow) >> Constants.SCALE_OFFSET;
-            if (absY & 0x2000 != 0) result = (result * pow) >> Constants.SCALE_OFFSET;
-            pow = (pow * pow) >> Constants.SCALE_OFFSET;
-            if (absY & 0x4000 != 0) result = (result * pow) >> Constants.SCALE_OFFSET;
-            pow = (pow * pow) >> Constants.SCALE_OFFSET;
-            if (absY & 0x8000 != 0) result = (result * pow) >> Constants.SCALE_OFFSET;
-            pow = (pow * pow) >> Constants.SCALE_OFFSET;
-            if (absY & 0x10000 != 0) result = (result * pow) >> Constants.SCALE_OFFSET;
-            pow = (pow * pow) >> Constants.SCALE_OFFSET;
-            if (absY & 0x20000 != 0) result = (result * pow) >> Constants.SCALE_OFFSET;
-            pow = (pow * pow) >> Constants.SCALE_OFFSET;
-            if (absY & 0x40000 != 0) result = (result * pow) >> Constants.SCALE_OFFSET;
-            pow = (pow * pow) >> Constants.SCALE_OFFSET;
-            if (absY & 0x80000 != 0) result = (result * pow) >> Constants.SCALE_OFFSET;
-
-            if (result == 0 || absY > 0xfffff) revert Math128x128__PowerUnderflow(x, y);
-
-            return invert ? type(uint256).max / result : result;
         }
+
+        if (absY < 0x100000) {
+            result = Constants.SCALE;
+            assembly {
+                let pow := x
+                if gt(x, 0xffffffffffffffffffffffffffffffff) {
+                    pow := div(not(0), pow)
+                    invert := not(invert)
+                }
+
+                if gt(and(absY, 0x1), 0) {
+                    result := shr(128, mul(result, pow))
+                }
+                pow := shr(128, mul(pow, pow))
+                if gt(and(absY, 0x2), 0) {
+                    result := shr(128, mul(result, pow))
+                }
+                pow := shr(128, mul(pow, pow))
+                if gt(and(absY, 0x4), 0) {
+                    result := shr(128, mul(result, pow))
+                }
+                pow := shr(128, mul(pow, pow))
+                if gt(and(absY, 0x8), 0) {
+                    result := shr(128, mul(result, pow))
+                }
+                pow := shr(128, mul(pow, pow))
+                if gt(and(absY, 0x10), 0) {
+                    result := shr(128, mul(result, pow))
+                }
+                pow := shr(128, mul(pow, pow))
+                if gt(and(absY, 0x20), 0) {
+                    result := shr(128, mul(result, pow))
+                }
+                pow := shr(128, mul(pow, pow))
+                if gt(and(absY, 0x40), 0) {
+                    result := shr(128, mul(result, pow))
+                }
+                pow := shr(128, mul(pow, pow))
+                if gt(and(absY, 0x80), 0) {
+                    result := shr(128, mul(result, pow))
+                }
+                pow := shr(128, mul(pow, pow))
+                if gt(and(absY, 0x100), 0) {
+                    result := shr(128, mul(result, pow))
+                }
+                pow := shr(128, mul(pow, pow))
+                if gt(and(absY, 0x200), 0) {
+                    result := shr(128, mul(result, pow))
+                }
+                pow := shr(128, mul(pow, pow))
+                if gt(and(absY, 0x400), 0) {
+                    result := shr(128, mul(result, pow))
+                }
+                pow := shr(128, mul(pow, pow))
+                if gt(and(absY, 0x800), 0) {
+                    result := shr(128, mul(result, pow))
+                }
+                pow := shr(128, mul(pow, pow))
+                if gt(and(absY, 0x1000), 0) {
+                    result := shr(128, mul(result, pow))
+                }
+                pow := shr(128, mul(pow, pow))
+                if gt(and(absY, 0x2000), 0) {
+                    result := shr(128, mul(result, pow))
+                }
+                pow := shr(128, mul(pow, pow))
+                if gt(and(absY, 0x4000), 0) {
+                    result := shr(128, mul(result, pow))
+                }
+                pow := shr(128, mul(pow, pow))
+                if gt(and(absY, 0x8000), 0) {
+                    result := shr(128, mul(result, pow))
+                }
+                pow := shr(128, mul(pow, pow))
+                if gt(and(absY, 0x10000), 0) {
+                    result := shr(128, mul(result, pow))
+                }
+                pow := shr(128, mul(pow, pow))
+                if gt(and(absY, 0x20000), 0) {
+                    result := shr(128, mul(result, pow))
+                }
+                pow := shr(128, mul(pow, pow))
+                if gt(and(absY, 0x40000), 0) {
+                    result := shr(128, mul(result, pow))
+                }
+                pow := shr(128, mul(pow, pow))
+                if gt(and(absY, 0x80000), 0) {
+                    result := shr(128, mul(result, pow))
+                }
+            }
+        }
+
+        // revert if y is too big or if x^y underflowed
+        if (result == 0) revert Math128x128__PowerUnderflow(x, y);
+
+        return invert ? type(uint256).max / result : result;
     }
 }

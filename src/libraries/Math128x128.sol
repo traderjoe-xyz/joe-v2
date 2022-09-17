c470484bdb10b5ee95bf88e17b927c8c86eeda58
0x0Louis <louis@joepegs.com>
2022-09-17 21:23:08 +0200

    issue n°201: log2 optimization

--- src/libraries/Math128x128.sol
+++ src/libraries/Math128x128.sol
@@ -11,6 +11,10 @@ library Math128x128 {
     using Math512Bits for uint256;
     using BitMath for uint256;
 
+    uint256 constant LOG_SCALE_OFFSET = 127;
+    uint256 constant LOG_SCALE = 1 << LOG_SCALE_OFFSET;
+    uint256 constant LOG_SCALE_SQUARED = LOG_SCALE * LOG_SCALE;
+
     /// @notice Calculates the binary logarithm of x.
     ///
     /// @dev Based on the iterative approximation algorithm.
@@ -20,54 +24,60 @@ library Math128x128 {
     /// - x must be greater than zero.
     ///
     /// Caveats:
-    /// - The results are not perfectly accurate to the last decimal, due to the lossy precision of the iterative approximation.
+    /// - The results are not perfectly accurate to the last decimal, due to the lossy precision of the iterative approximation
+    /// Also because x is converted to an unsigned 129.127-binary fixed-point number during the operation to optimize the multiplication
     ///
     /// @param x The unsigned 128.128-binary fixed-point number for which to calculate the binary logarithm.
     /// @return result The binary logarithm as a signed 128.128-binary fixed-point number.
     function log2(uint256 x) internal pure returns (int256 result) {
+        // Convert x to a unsigned 129.127-binary fixed-point number to optimize the multiplication.
+        // If we use an offset of 128 bits, y would need 129 bits and y**2 would would overflow and we would have to
+        // use mulDiv, by reducing x to 129.127-binary fixed-point number we assert that y will use 128 bits, and we
+        // can use the regular multiplication
+        x >>= 1;
+
         if (x == 0) revert Math128x128__LogUnderflow();
 
         unchecked {
             // This works because log2(x) = -log2(1/x).
             int256 sign;
-            if (x >= Constants.SCALE) {
+            if (x >= LOG_SCALE) {
                 sign = 1;
             } else {
                 sign = -1;
-                // Do the fixed-point inversion inline to save gas. The numerator is Constants.SCALE * Constants.S_SCALE.
-                x = type(uint256).max / x;
+                // Do the fixed-point inversion inline to save gas
+                x = LOG_SCALE_SQUARED / x;
             }
 
             // Calculate the integer part of the logarithm and add it to the result and finally calculate y = x * 2^(-n).
-            uint256 n = (x >> Constants.SCALE_OFFSET).mostSignificantBit();
+            uint256 n = (x >> LOG_SCALE_OFFSET).mostSignificantBit();
 
-            // The integer part of the logarithm as a signed 128.128-binary fixed-point number. The operation can't overflow
-            // because n is maximum 255, Constants.SCALE is 2^128 and sign is either 1 or -1.
-            result = int256(n) << Constants.SCALE_OFFSET;
+            // The integer part of the logarithm as a signed 129.127-binary fixed-point number. The operation can't overflow
+            // because n is maximum 255, LOG_SCALE_OFFSET is 127 bits and sign is either 1 or -1.
+            result = int256(n) << LOG_SCALE_OFFSET;
 
             // This is y = x * 2^(-n).
             uint256 y = x >> n;
 
             // If y = 1, the fractional part is zero.
-            if (y == Constants.SCALE) {
-                return result * sign;
-            }
+            if (y != LOG_SCALE) {
+                // Calculate the fractional part via the iterative approximation.
+                // The "delta >>= 1" part is equivalent to "delta /= 2", but shifting bits is faster.
+                for (int256 delta = int256(1 << (LOG_SCALE_OFFSET - 1)); delta > 0; delta >>= 1) {
+                    y = (y * y) >> LOG_SCALE_OFFSET;
 
-            // Calculate the fractional part via the iterative approximation.
-            // The "delta >>= 1" part is equivalent to "delta /= 2", but shifting bits is faster.
-            for (int256 delta = int256(1 << (Constants.SCALE_OFFSET - 1)); delta > 0; delta >>= 1) {
-                y = y.mulShiftRoundDown(y, Constants.SCALE_OFFSET);
+                    // Is y^2 > 2 and so in the range [2,4)?
+                    if (y >= 1 << (LOG_SCALE_OFFSET + 1)) {
+                        // Add the 2^(-m) factor to the logarithm.
+                        result += delta;
 
-                // Is y^2 > 2 and so in the range [2,4)?
-                if (y >= 1 << (Constants.SCALE_OFFSET + 1)) {
-                    // Add the 2^(-m) factor to the logarithm.
-                    result += delta;
-
-                    // Corresponds to z/2 on Wikipedia.
-                    y >>= 1;
+                        // Corresponds to z/2 on Wikipedia.
+                        y >>= 1;
+                    }
                 }
             }
-            result *= sign;
+            // Convert x back to unsigned 128.128-binary fixed-point number
+            result = (result * sign) << 1;
         }
     }
 

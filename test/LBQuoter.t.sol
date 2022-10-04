b12cdf717354aca68dab7ba91d2ed2ab6ee46039
Patate <patate@joepegs.com>
2022-10-04 14:46:41 +0200

    add tests for quoter

--- test/LBQuoter.t.sol
+++ test/LBQuoter.t.sol
@@ -5,8 +5,12 @@ pragma solidity ^0.8.0;
 import "./TestHelper.sol";
 
 contract LiquidityBinQuoterTest is TestHelper {
+    using Math512Bits for uint256;
+
     IJoeFactory private factoryV1;
     ERC20MockDecimals private testWavax;
+
+    uint256 private defaultBaseFee = DEFAULT_BIN_STEP * uint256(DEFAULT_BASE_FACTOR) * 1e10;
 
     function setUp() public {
         vm.createSelectFork(vm.rpcUrl("avalanche"), 19_358_000);
@@ -35,17 +39,10 @@ contract LiquidityBinQuoterTest is TestHelper {
         usdt.approve(address(routerV1), type(uint256).max);
 
         // Create pairs
-        factoryV1.createPair(address(usdc), address(testWavax));
+        factoryV1.createPair(address(testWavax), address(usdc));
         factoryV1.createPair(address(usdt), address(usdc));
-
-        createLBPairDefaultFeesFromStartId(usdc, testWavax, convertIdAvaxToUSD(DEFAULT_BIN_STEP));
         createLBPairDefaultFees(usdt, usdc);
-
-        // Add Liquidity
-        routerV1.addLiquidity(address(usdc), address(testWavax), 20_000e6, 1_000e18, 0, 0, DEV, block.timestamp);
-        routerV1.addLiquidity(address(usdt), address(usdc), 1_000e6, 1_000e6, 0, 0, DEV, block.timestamp);
-        addLiquidityFromRouter(usdt, usdc, 10_000e6, ID_ONE, 9, 2, DEFAULT_BIN_STEP);
-        addLiquidityFromRouter(usdc, testWavax, 100e18, convertIdAvaxToUSD(DEFAULT_BIN_STEP), 9, 2, DEFAULT_BIN_STEP);
+        createLBPairDefaultFeesFromStartId(testWavax, usdc, convertIdAvaxToUSD(DEFAULT_BIN_STEP));
     }
 
     function testConstructor() public {
@@ -54,41 +51,159 @@ contract LiquidityBinQuoterTest is TestHelper {
         assertEq(address(quoter.factoryV2()), address(factory));
     }
 
-    function testGetSwapOut() public {
+    function testGetSwapOutOnV2Pair() public {
+        addLiquidityFromRouter(testWavax, usdc, 20_000e6, convertIdAvaxToUSD(DEFAULT_BIN_STEP), 9, 2, DEFAULT_BIN_STEP);
+
         address[] memory route;
-        route = new address[](3);
+        route = new address[](2);
         route[0] = address(testWavax);
         route[1] = address(usdc);
-        route[2] = address(usdt);
 
-        LBQuoter.Quote memory quote = quoter.findBestPathAmountIn(route, 1e18);
+        LBQuoter.Quote memory quote = quoter.findBestPathFromAmountIn(route, 1e18);
 
-        assertApproxEqAbs(quote.amounts[2], 20e6, 2e6, "Price of 1 AVAX should be approx 20 USDT");
+        assertApproxEqAbs(quote.amounts[1], 20e6, 2e6, "Price of 1 AVAX should be approx 20 USDC");
+        assertEq(quote.binSteps[0], DEFAULT_BIN_STEP);
+        assertApproxEqAbs(quote.fees[0], defaultBaseFee, 1e14);
+        assertApproxEqAbs(quote.amounts[1], quote.virtualAmountsWithoutSlippage[1], 1e14);
     }
 
-    function testGetSwapIn() public {
+    function testGetSwapInOnV2Pair() public {
+        addLiquidityFromRouter(testWavax, usdc, 20_000e6, convertIdAvaxToUSD(DEFAULT_BIN_STEP), 9, 2, DEFAULT_BIN_STEP);
+
+        address[] memory route;
+        route = new address[](2);
+        route[0] = address(testWavax);
+        route[1] = address(usdc);
+
+        LBQuoter.Quote memory quote = quoter.findBestPathFromAmountOut(route, 20e6);
+
+        assertApproxEqAbs(quote.amounts[0], 1e18, 0.1e18, "Price of 1 AVAX should be approx 20 USDT");
+        assertEq(quote.binSteps[0], DEFAULT_BIN_STEP);
+        assertApproxEqAbs(quote.fees[0], defaultBaseFee, 1e14);
+        assertApproxEqAbs(quote.amounts[0], quote.virtualAmountsWithoutSlippage[0], 1e16);
+    }
+
+    function testGetSwapOutOnV1Pair() public {
+        routerV1.addLiquidity(address(testWavax), address(usdc), 1_000e18, 20_000e6, 0, 0, DEV, block.timestamp);
+
+        address[] memory route;
+        route = new address[](2);
+        route[0] = address(testWavax);
+        route[1] = address(usdc);
+
+        LBQuoter.Quote memory quote = quoter.findBestPathFromAmountIn(route, 1e18);
+
+        assertApproxEqAbs(quote.amounts[1], 20e6, 2e6, "Price of 1 AVAX should be approx 20 USDC");
+        assertEq(quote.binSteps[0], 0);
+        assertEq(quote.fees[0], 0.003e18);
+        assertApproxEqAbs(quote.amounts[1], quote.virtualAmountsWithoutSlippage[1], 1e16);
+    }
+
+    function testGetSwapInOnV1Pair() public {
+        routerV1.addLiquidity(address(testWavax), address(usdc), 1_000e18, 20_000e6, 0, 0, DEV, block.timestamp);
+
+        address[] memory route;
+        route = new address[](2);
+        route[0] = address(testWavax);
+        route[1] = address(usdc);
+
+        LBQuoter.Quote memory quote = quoter.findBestPathFromAmountOut(route, 20e6);
+
+        assertApproxEqAbs(quote.amounts[0], 1e18, 0.1e18, "Price of 1 AVAX should be approx 20 USDT");
+        assertEq(quote.binSteps[0], 0);
+        assertEq(quote.fees[0], 0.003e18);
+        assertApproxEqAbs(quote.amounts[0], quote.virtualAmountsWithoutSlippage[0], 1e16);
+    }
+
+    function testGetSwapOutOnComplexRoute() public {
+        routerV1.addLiquidity(address(testWavax), address(usdc), 1_000e18, 20_000e6, 0, 0, DEV, block.timestamp);
+        addLiquidityFromRouter(usdt, usdc, 10_000e6, ID_ONE, 9, 2, DEFAULT_BIN_STEP);
+
         address[] memory route;
         route = new address[](3);
         route[0] = address(testWavax);
         route[1] = address(usdc);
         route[2] = address(usdt);
 
-        LBQuoter.Quote memory quote = quoter.findBestPathAmountOut(route, 20e6);
+        LBQuoter.Quote memory quote = quoter.findBestPathFromAmountIn(route, 1e18);
+
+        assertApproxEqAbs(quote.amounts[2], 20e6, 2e6, "Price of 1 AVAX should be approx 20 USDT");
+        assertEq(quote.binSteps[0], 0);
+        assertEq(quote.binSteps[1], DEFAULT_BIN_STEP);
+        assertEq(quote.fees[0], 0.003e18);
+        assertApproxEqAbs(quote.fees[1], defaultBaseFee, 1e14);
+        assertApproxEqAbs(quote.amounts[2], quote.virtualAmountsWithoutSlippage[2], 1e16);
+    }
+
+    function testGetSwapInOnComplexRoute() public {
+        routerV1.addLiquidity(address(testWavax), address(usdc), 1_000e18, 20_000e6, 0, 0, DEV, block.timestamp);
+        addLiquidityFromRouter(usdt, usdc, 10_000e6, ID_ONE, 9, 2, DEFAULT_BIN_STEP);
+
+        address[] memory route;
+        route = new address[](3);
+        route[0] = address(testWavax);
+        route[1] = address(usdc);
+        route[2] = address(usdt);
+
+        LBQuoter.Quote memory quote = quoter.findBestPathFromAmountOut(route, 20e6);
 
         assertApproxEqAbs(quote.amounts[0], 1e18, 0.1e18, "Price of 1 AVAX should be approx 20 USDT");
+
+        assertEq(quote.binSteps[0], 0);
+        assertEq(quote.binSteps[1], DEFAULT_BIN_STEP);
+        assertEq(quote.fees[0], 0.003e18);
+        assertApproxEqAbs(quote.fees[1], defaultBaseFee, 1e14);
+        assertApproxEqAbs(quote.amounts[0], quote.virtualAmountsWithoutSlippage[0], 1e16);
+    }
+
+    function testGetSwapInWithMultipleChoices() public {
+        // On V1, 1 AVAX = 19 USDC
+        routerV1.addLiquidity(address(testWavax), address(usdc), 1_000e18, 19_000e6, 0, 0, DEV, block.timestamp);
+        // On V2, 1 AVAX = 20 USDC
+        addLiquidityFromRouter(testWavax, usdc, 20_000e6, convertIdAvaxToUSD(DEFAULT_BIN_STEP), 9, 2, DEFAULT_BIN_STEP);
+
+        address[] memory route;
+        route = new address[](2);
+        route[0] = address(testWavax);
+        route[1] = address(usdc);
+
+        LBQuoter.Quote memory quote = quoter.findBestPathFromAmountOut(route, 20e6);
+
+        assertApproxEqAbs(quote.amounts[0], 1e18, 0.1e18, "Price of 1 AVAX should be approx 20 USDC");
+        assertEq(quote.binSteps[0], DEFAULT_BIN_STEP, "LBPair should be picked as it has the best price");
+    }
+
+    function testGetSwapOutWithMultipleChoices() public {
+        // On V1, 1 AVAX = 20 USDT
+        routerV1.addLiquidity(address(testWavax), address(usdt), 1_000e18, 20_000e6, 0, 0, DEV, block.timestamp);
+        // On V2, 1 AVAX = 19 USDT
+        uint24 desiredId = getIdFromPrice(uint256(19e6).shiftDivRoundDown(128, 1e18));
+        createLBPairDefaultFeesFromStartId(testWavax, usdt, desiredId);
+        addLiquidityFromRouter(testWavax, usdt, 20_000e6, desiredId, 9, 2, DEFAULT_BIN_STEP);
+
+        address[] memory route;
+        route = new address[](2);
+        route[0] = address(testWavax);
+        route[1] = address(usdt);
+
+        LBQuoter.Quote memory quote = quoter.findBestPathFromAmountIn(route, 1e18);
+
+        assertApproxEqAbs(quote.amounts[1], 20e6, 2e6, "Price of 1 AVAX should be approx 20 USDT");
+        assertEq(quote.binSteps[0], 0, "V1 pair should be picked as it has the best price");
     }
 
     function testInvalidLength() public {
         address[] memory route;
         route = new address[](1);
         vm.expectRevert(LBQuoter_InvalidLength.selector);
-        quoter.findBestPathAmountIn(route, 1e18);
+        quoter.findBestPathFromAmountIn(route, 1e18);
         vm.expectRevert(LBQuoter_InvalidLength.selector);
-        quoter.findBestPathAmountOut(route, 20e6);
+        quoter.findBestPathFromAmountOut(route, 20e6);
     }
 
     function convertIdAvaxToUSD(uint16 _binStep) internal pure returns (uint24 id) {
-        id = getIdFromPrice(20e48, _binStep);
+        uint256 price = uint256(20e6).shiftDivRoundDown(128, 1e18);
+        id = getIdFromPrice(price, _binStep);
     }
 
     function getIdFromPrice(uint256 _price, uint16 _binStep) internal pure returns (uint24 id) {

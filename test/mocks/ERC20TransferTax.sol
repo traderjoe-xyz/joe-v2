6ad4a497872b77a8a6d8b4a760b3c80db130cc7d
Patate <patate@joepegs.com>
2023-01-23 10:03:22 +0100

    refactor ERC20TransferTaxMock

--- test/mocks/ERC20TransferTax.sol
+++ test/mocks/ERC20TransferTax.sol
@@ -11,19 +11,9 @@ contract ERC20TransferTaxMock is ERC20Mock {
     /// @dev Constructor
     constructor() ERC20Mock(18) {}
 
-    function transferFrom(address from, address to, uint256 amount) public override returns (bool) {
-        address spender = _msgSender();
-        _spendAllowance(from, spender, amount);
+    function _transfer(address from, address to, uint256 amount) internal override {
         uint256 tax = amount / 2;
-        _transfer(from, to, amount - tax);
         _burn(from, tax);
-        return true;
-    }
-
-    function transfer(address to, uint256 amount) public override returns (bool) {
-        uint256 tax = amount / 2;
-        _transfer(msg.sender, to, amount - tax);
-        _burn(msg.sender, tax);
-        return true;
+        super._transfer(from, to, amount - tax);
     }
 }

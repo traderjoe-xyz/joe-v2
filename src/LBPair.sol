33f30dc626d5442f3bd48ca2ab55330f42fe4fec
0x0Louis <louis@joepegs.com>
2023-02-01 14:39:09 +0100

    small fix on pair

--- src/LBPair.sol
+++ src/LBPair.sol
@@ -17,12 +17,12 @@ import {OracleHelper} from "./libraries/OracleHelper.sol";
 import {PackedUint128Math} from "./libraries/math/PackedUint128Math.sol";
 import {PairParameterHelper} from "./libraries/PairParameterHelper.sol";
 import {PriceHelper} from "./libraries/PriceHelper.sol";
-import {ReentrancyGuardUpgradeable} from "./libraries/ReentrancyGuardUpgradeable.sol";
+import {ReentrancyGuard} from "./libraries/ReentrancyGuard.sol";
 import {SafeCast} from "./libraries/math/SafeCast.sol";
 import {TreeMath} from "./libraries/math/TreeMath.sol";
 import {Uint256x256Math} from "./libraries/math/Uint256x256Math.sol";
 
-contract LBPair is LBToken, ReentrancyGuardUpgradeable, Clone, ILBPair {
+contract LBPair is LBToken, ReentrancyGuard, Clone, ILBPair {
     using BinHelper for bytes32;
     using FeeHelper for uint128;
     using LiquidityConfigurations for bytes32;
@@ -491,7 +491,7 @@ contract LBPair is LBToken, ReentrancyGuardUpgradeable, Clone, ILBPair {
 
         if (amountsOut == 0) revert LBPair__InsufficientAmountOut();
 
-        _oracle.update(parameters, activeId);
+        parameters = _oracle.update(parameters, activeId);
 
         _reserves = reserves.sub(amountsOut);
         _parameters = parameters.setActiveId(activeId);
@@ -888,8 +888,8 @@ contract LBPair is LBToken, ReentrancyGuardUpgradeable, Clone, ILBPair {
                     _protocolFees = _protocolFees.add(protocolCFees);
                 }
 
+                parameters = _oracle.update(parameters, id);
                 _parameters = parameters;
-                _oracle.update(parameters, id);
 
                 emit CompositionFees(msg.sender, id, fees, protocolCFees);
             }

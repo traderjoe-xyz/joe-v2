6c33c49802b53fc728b0ce05472df0e09cb51461
0x0Louis <louis@joepegs.com>
2024-02-21 16:29:11 +0100

    PR comments

--- src/interfaces/ILBFactory.sol
+++ src/interfaces/ILBFactory.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.10;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import {ILBHooks} from "../interfaces/ILBHooks.sol";
+import {ILBHooks} from "./ILBHooks.sol";
 import {ILBPair} from "./ILBPair.sol";
 
 /**
